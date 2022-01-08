beff8bfd7018fa4eca35968ce311f5dae5a78232
Krystian Postek <krystian@postek.eu>
2022-01-08 16:16:52 +0100

    Minor changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,23 +2,54 @@ import React from "react";
 import Root from "../components/sassy";
 import * as indexStyles from '../components/index.module.sass'
 import Vl from "../components/vl";
+import {Link} from "gatsby";
 
-function CategoryBlock({name}) {
+function CategoryBlock({name, children}) {
   return (
     <div className={indexStyles.categoryBlock}>
-      <p style={{margin: '10px 0'}}>{name}</p>
-      <p style={{opacity: 0.5, margin: '5px 0', letterSpacing: '-0.5px'}}>{name}</p>
-      <p style={{opacity: 0.2, margin: '2px 0', letterSpacing: '-1px'}}>{name}</p>
+      <p style={{margin: '10px 0', fontSize: '16pt', letterSpacing: '1px'}}>{name}</p>
+      {children}
     </div>
   )
 }
+
+function GithubRepo({path, title, children}) {
+  return (
+    <div style={{paddingTop: '8px'}}>
+      <h4 style={{margin: '4px 0', letterSpacing: '1px'}}>
+        <Link to={`https://github.com/${path}`}>
+          {title}
+        </Link>
+      </h4>
+      <p style={{maxWidth: '20vw', margin: '0', fontSize: '11pt'}}>
+        {children}
+      </p>
+    </div>
+  )
+}
+
+/* export const query = graphql`
+  query MyQuery {
+    allCosmicjsNotes(limit: 3) {
+      edges {
+        node {
+          created_at
+          title
+          metadata {
+            summary
+          }
+        }
+      }
+    }
+  }`
+*/
 
 function TimelineBlock({title, year, children}) {
   return (
     <div className={indexStyles.timelineBlock}>
       <div style={{textAlign: "right", flex: '1', width: '40vw'}}>
         <div style={{paddingRight: '10px'}}>
-          <h2>{title}</h2>
+          <h2 style={{letterSpacing: '1px'}}>{title}</h2>
           <h3 className={indexStyles.date}>{year}</h3>
         </div>
       </div>
@@ -31,20 +62,58 @@ function TimelineBlock({title, year, children}) {
   )
 }
 
-export default function indexPage () {
+export default function indexPage({data}) {
   return (
     <Root>
-      <div style={{display: "flex", flexDirection: "column",  alignItems: "center", justifyContent: "center", height: '88vh'}}>
+      <header style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: '88vh'
+      }}>
         <h1 style={{textAlign: "center", alignSelf: "center"}} className={indexStyles.welcomeTitle}>K. Postek</h1>
-        <p className={indexStyles.someSmartPhrase}>Small developer, big dreamer</p>
+        <p style={{margin: '4px'}} className={indexStyles.someSmartPhrase}>Small developer, big dreamer</p>
+        <p style={{margin: '4px'}}>Full stack, Python, JS/TS, React, Docker, MongoDB, Swift</p>
         <hr className={indexStyles.titleHrLine}/>
         <div className={indexStyles.categoryBlockRoot}>
-          <CategoryBlock name={'blog'}/>
-          <CategoryBlock name={'projects'}/>
-          <CategoryBlock name={'prototypes'}/>
-          <CategoryBlock name={'contact'}/>
+          {/*<CategoryBlock name={'blog'}>
+            <p>latest posts</p>
+            {data.allCosmicjsNotes.edges.map((e) => <p><span style={{fontWeight: 'bold'}}>{e.node.title}</span> <small>{e.node.metadata.summary}</small></p>)}
+          </CategoryBlock>*/}
+          <CategoryBlock name={'projects'}>
+            <GithubRepo path={'pjatk21/pjatk-cat-guard'} title={'Gadoneko'}>
+              full stack discord bot created for managing and moderating discord servers
+            </GithubRepo>
+            <GithubRepo path={'kpostekk/AwakeThat'} title={'Awake That'}>
+              iOS app for WOL based on SwiftUI and CoreData
+            </GithubRepo>
+            <GithubRepo path={'kpostekk/ezcliy'} title={'ezcliy'}>
+              class oriented cli creation tool
+            </GithubRepo>
+          </CategoryBlock>
+          <CategoryBlock name={'prototypes'}>
+            <GithubRepo path={'kpostekk/Mini-Gakko'} title={'Mini Gakko'}>
+              iOS app replacement of horrible web app of my collage website, no longer maintained due to API's security issues
+            </GithubRepo>
+            <GithubRepo path={'kpostekk/opencore-composer'} title={'OpenCore composer'}>
+              tool for creating config.plist with predefined rules, inspired by docker-compose
+            </GithubRepo>
+          </CategoryBlock>
+          <CategoryBlock name={'contact'}>
+            <p style={{margin: 0, paddingTop: '12px'}}>
+              <Link to={'mailto:krystian@postek.eu'}>
+                krystian@postek.eu
+              </Link>
+            </p>
+            <p>
+              <Link to={'https://t.me/kpostekk'}>
+                telegram @kpostekk
+              </Link>
+            </p>
+          </CategoryBlock>
         </div>
-      </div>
+      </header>
       <div style={{display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>
         <p style={{opacity: 0}} className={indexStyles.appearAnimation}>scroll down</p>
         <h1 style={{fontWeight: 300, marginTop: 0}}>My timeline</h1>
@@ -55,10 +124,13 @@ export default function indexPage () {
         <TimelineBlock title={'college'} year={'october-present 2021'}>
           <p>Student of IT at <a href={'https://www.pja.edu.pl/en/'}>PJAIT</a></p>
         </TimelineBlock>
-        <h2 style={{fontStyle: 'italic', fontWeight: 300}}>future plans</h2>
+        <h2 style={{fontStyle: 'italic', fontWeight: 300, margin: 0, letterSpacing: '1px'}}>future plans</h2>
         <TimelineBlock title={'college graduation'} year={'2025'}>
           <p>Graduating with software engineering degree</p>
         </TimelineBlock>
+      </div>
+      <div style={{padding: '10px'}}>
+        <p style={{textAlign: 'center', padding: 0, fontSize: '9pt', letterSpacing: '1px'}}>Powered by Gatsby and Cosmic</p>
       </div>
     </Root>
   )
